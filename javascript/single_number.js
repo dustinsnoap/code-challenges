8a2efcf830dc4cbc3458a0a120bf5ca832875885
dustinsnoap <afrobacon@gmail.com>
2020-04-10 11:23:11 -0400

    js: single_number

---
+++ javascript/single_number.js
@@ -0,0 +1,24 @@
+//Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+//Note:
+//Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+//Example 1:
+//Input: [2,2,1]
+//Output: 1
+
+//Example 2:
+//Input: [4,1,2,1,2]
+//Output: 4
+
+// #Explanation:
+//^= is the XOR opporator
+//any number XOR 0 is the same number
+//any number XOR itself is 0
+//if we XOR every number together only the odd one out should remain
+
+const singleNumber = nums => {
+    let bit = 0
+    for(i=0; i<nums.length; i++) bit ^= nums[i]
+    return bit
+}
