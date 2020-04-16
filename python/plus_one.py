1434d7553f1eab7b65b48e8d50e069f4534a4125
dustinsnoap <afrobacon@gmail.com>
2020-04-15 22:16:23 -0400

    python: plus_one

---
+++ python/plus_one.py
@@ -0,0 +1,32 @@
+# Given a non-empty array of digits representing a non-negative integer, 
+# plus one to the integer.
+# The digits are stored such that the most significant digit is at the head of the list,
+# and each element in the array contain a single digit.
+# You may assume the integer does not contain any leading zero, 
+# except the number 0 itself.
+
+# Example 1:
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+
+# Example 2:
+# Input: [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+
+def plusOne(digits):
+    for i in reversed(range(len(digits))):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    if digits[0] == 0: #edge case where the number is all 9s
+        digits.insert(0,1)
+    return digits
+
+ex1 = [1,2,3] #[1,2,4]
+ex2 = [4,3,2,1] #[4,3,2,2]
+
+print(plusOne(ex1))
+print(plusOne(ex2))
