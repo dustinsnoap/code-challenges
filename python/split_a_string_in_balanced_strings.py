f091e763a04669e7e54508c12e2448b79a00c4dc
dustinsnoap <dustinsnoap@gmail.com>
2020-09-08 03:06:55 -0400

    python: split_a_string_in_balanced_string time: 668s runtime: 28ms 81.81% memory: 13.8mb 51.97%

---
+++ python/split_a_string_in_balanced_strings.py
@@ -0,0 +1,48 @@
+# Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+# Given a balanced string s split it in the maximum amount of balanced strings.
+# Return the maximum amount of splitted balanced strings.
+
+# Example 1:
+# Input: s = "RLRRLLRLRL"
+# Output: 4
+# Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+# Example 2:
+
+# Input: s = "RLLLLRRRLR"
+# Output: 3
+# Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+# Example 3:
+
+# Input: s = "LLLLRRRR"
+# Output: 1
+# Explanation: s can be split into "LLLLRRRR".
+# Example 4:
+
+# Input: s = "RLRRRLLRLL"
+# Output: 2
+# Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+
+# Constraints:
+# 1 <= s.length <= 1000
+# s[i] = 'L' or 'R'
+
+def balancedStringSplit(s):
+    counter = 0
+    leftcount = 0
+    rightcount = 0
+    for c in s:
+        if c == 'L': leftcount += 1
+        else: rightcount += 1
+        if leftcount == rightcount:
+            leftcount = 0
+            rightcount = 0
+            counter += 1
+    return counter
+
+examples = ["RLRRLLRLRL","RLLLLRRRLR","LLLLRRRR","RLRRRLLRLL"]
+answers = [4,3,1,2]
+
+for idx, num in enumerate(examples):
+    print('guess:', balancedStringSplit(num), 'ans:', answers[idx])
+
+
