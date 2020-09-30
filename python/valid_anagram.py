955306578e0a431c4ca74aa8603eabd9ec844bdd
dustinsnoap <dustinsnoap@gmail.com>
2020-09-30 01:59:39 -0400

    Valid Anagram language: python difficulty: easy time: 426 runtime: 52ms 55.89% memory: 14.3mb 39.3%

---
+++ python/valid_anagram.py
@@ -0,0 +1,33 @@
+# Given two strings s and t , write a function to determine if t is an anagram of s.
+
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+
+# Note:
+# You may assume the string contains only lowercase alphabets.
+
+# Follow up:
+# What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+def isAnagram(s, t):
+    if len(s) != len(t): return False
+    chars = {}
+    for i in range(len(s)):
+        if s[i] not in chars: chars[s[i]] = 1
+        else: chars[s[i]] += 1
+        if t[i] not in chars: chars[t[i]] = -1
+        else: chars[t[i]] -= 1
+    for char in chars:
+        if chars[char] != 0: return False
+    return True
+
+inputs = [{'s': "anagram", 't': "nagaram"}, {'s': "rat", 't': "car"}]
+outputs = [True, False]
+
+for i in inputs:
+    print(isAnagram(s=i['s'], t=i['t']))
