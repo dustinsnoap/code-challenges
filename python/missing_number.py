6252a689b23b7f27cc0efa9cdde3800b90f56bc3
dustinsnoap <dustinsnoap@gmail.com>
2020-10-02 00:13:04 -0400

    Missing Number language: python difficulty: easy time: 460 runtime: 192ms 25.26% memory: 15.2mb 23.86%

---
+++ python/missing_number.py
@@ -0,0 +1,39 @@
+# Given an array nums containing n distinct numbers taken from 0, 1, 2, ..., n, return the one that is missing from the array.
+# Follow up: Could you implement a solution using only constant extra space complexity and linear runtime complexity?
+
+# Example 1:
+# Input: nums = [3,0,1]
+# Output: 2
+
+# Example 2:
+# Input: nums = [0,1]
+# Output: 2
+
+# Example 3:
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
+
+# Example 4:
+# Input: nums = [0]
+# Output: 1
+
+# Constraints:
+# n == nums.length
+# 1 <= n <= 104
+# 0 <= nums[i] <= n
+# All the numbers of nums are unique.
+
+def missingNumber(nums):
+    expected = [None] * (len(nums)+1)
+    for n in nums: expected[n] = n
+    # print(expected)
+    for i in range(len(expected)):
+        if expected[i] == None: return i
+    return False
+
+from tools import test
+inputs = [[3,0,1],[0,1],[9,6,4,2,3,5,7,0,1],[0]]
+outputs = [2,2,8,1]
+funcs = [missingNumber]
+
+test(inputs, outputs, funcs)
