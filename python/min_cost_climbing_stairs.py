c6a1091f7d923667add7ffb8d4465d5de1feb537
dustinsnoap <dustinsnoap@gmail.com>
2020-10-03 17:17:47 -0400

    Min Cost Climbing Stairs language: python difficulty: easy runtime: 56ms 81.31% memory: 14.3mb 15.16%

---
+++ python/min_cost_climbing_stairs.py
@@ -0,0 +1,35 @@
+# On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+# Once you pay the cost, you can either climb one or two steps. 
+# You need to find minimum cost to reach the top of the floor,
+# and you can either start from the step with index 0, or the step with index 1.
+
+# Example 1:
+# Input: cost = [10, 15, 20]
+# Output: 15
+# Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+
+# Example 2:
+# Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+# Output: 6
+# Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+
+# Note:
+# cost will have a length in the range [2, 1000].
+# Every cost[i] will be an integer in the range [0, 999].
+
+def solution1(cost):
+    if len(cost) < 2: return 0
+    steps = {0: cost[0]}
+    last_step = len(cost)-1
+    for n in range(1, len(cost)):
+        if n-2 in steps: steps[n] = min(steps[n-1],steps[n-2]) + cost[n]
+        else: steps[n] = cost[n]
+    return min(steps[last_step],steps[last_step-1])
+
+
+from tools import test
+inputs = [[10,15,20],[1,100,1,1,1,100,1,1,100,1]]
+outputs = [15,6]
+funcs = [solution1]
+
+test(inputs, outputs, funcs)
